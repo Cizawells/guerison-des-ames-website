0457adb9016403a7d3ff7ea6bff9ad361413f235
Cizawells <cizawells70@gmail.com>
2025-03-15 09:59:40 +0100

    working

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.sanity.io'],
-      },
-}
-
-module.exports = nextConfig
+      domains: ['cdn.sanity.io'], // Allows Sanity images
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/studio/:path*',
+          destination: '/sanity/:path*', // Redirects requests to the Sanity Studio
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
