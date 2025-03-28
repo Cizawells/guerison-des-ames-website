4d75441a5ece6a013beb367c39d8bcaa3aadee3f
ciza Wells <cizawells70@gmail.com>
2025-03-28 14:48:37 +0200

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import "@/assets/css/responsive.css"; // Add this line to import globally
 import HomeOne from "@/components/homes/home-two";
-import "@/assets/css/responsive.css";  // Add this line to import globally
 
 import Wrapper from "@/layout/Wrapper";
-import { sanityClient } from "../../sanity.client";
 import { ToastContainer } from "react-toastify";
+import { sanityClient } from "../../sanity.client";
 
 export const metadata = {
   title: "Guerison des ames church",
@@ -11,25 +11,40 @@ export const metadata = {
 
 async function getHeroData() {
   const heroQuery = `*[_type == "heroSlider"][0]`;
-  const leadershipQuery = `*[_type == "leadership"]`;
+  const leadershipQuery = `*[_type == "leadership"] | order(_createdAt desc)`;
   const ProgramsQuery = `*[_type == "programs"]`;
 
-  const heroData = await sanityClient.fetch(heroQuery, {}, { next: { revalidate: 10 } });
-  const leadershipData = await sanityClient.fetch(leadershipQuery, {}, { next: { revalidate: 10 } });
-  const programsData = await sanityClient.fetch(ProgramsQuery, {}, { next: { revalidate: 10 } });
+  const heroData = await sanityClient.fetch(
+    heroQuery,
+    {},
+    { next: { revalidate: 10 } }
+  );
+  const leadershipData = await sanityClient.fetch(
+    leadershipQuery,
+    {},
+    { cache: "no-store" }
+  );
+  const programsData = await sanityClient.fetch(
+    ProgramsQuery,
+    {},
+    { next: { revalidate: 10 } }
+  );
 
-  return { heroData, leadershipData, programsData};
+  return { heroData, leadershipData, programsData };
 }
 
 export default async function Index() {
   const { heroData, leadershipData, programsData } = await getHeroData(); // ✅ Fetching data inside Server Component
 
-
   return (
     <Wrapper>
-            <ToastContainer position="top-center" autoClose={3000} />
+      <ToastContainer position="top-center" autoClose={3000} />
 
-      <HomeOne heroData={heroData} leadershipData={leadershipData} programsData={programsData}/>
+      <HomeOne
+        heroData={heroData}
+        leadershipData={leadershipData}
+        programsData={programsData}
+      />
     </Wrapper>
   );
 }
