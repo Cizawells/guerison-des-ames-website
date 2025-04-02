d19c2732e5ac38a2a64bb2ddd533f0bdc9ac7180
ciza Wells <cizawells70@gmail.com>
2025-04-02 09:09:00 +0200

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,12 @@ import { ToastContainer } from "react-toastify";
 import { sanityClient } from "../../sanity.client";
 
 export const metadata = {
-  title: "Guerison des ames church Nyakabiga",
+  title: "Guerison des ames church",
 };
 
 async function getHeroData() {
   const heroQuery = `*[_type == "heroSlider"][0]`;
-  const leadershipQuery = `*[_type == "leadership"] | order(name == "Mberabagabo Manasse", _createdAt desc)`;
-
+  const leadershipQuery = `*[_type == "leadership"] | order(_createdAt desc)`;
   const ProgramsQuery = `*[_type == "programs"]`;
 
   const heroData = await sanityClient.fetch(
@@ -23,7 +22,7 @@ async function getHeroData() {
   const leadershipData = await sanityClient.fetch(
     leadershipQuery,
     {},
-    {  cache: "no-store" }
+    { next: { revalidate: 10 } }
   );
   const programsData = await sanityClient.fetch(
     ProgramsQuery,
